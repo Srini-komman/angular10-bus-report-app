3a0802d1435a38a9ed367e536be55e1c00f66a78
Srini Komman <srini.komman@gmail.com>
2020-10-14 08:41:38 +1100

    new pipe has been added to format the first 3 digits of route variant

---
+++ src/app/pipes/partial-bold.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PartialBoldPipe } from './partial-bold.pipe';
+
+describe('PartialBoldPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PartialBoldPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
