ed1b1604b2933361f6f685c9bd9548392fa9b648
Srini Komman <srini.komman@gmail.com>
2020-10-14 03:34:11 +1100

    bus reorganization and details components are added to show Bus report

---
+++ src/app/components/bus-organisations/bus-organisations.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BusReportsService } from 'src/app/services/bus-reports.service';
+import { BusOrganisations } from 'src/app/interfaces/bus-organisations.model';
+
+@Component({
+  selector: 'app-bus-organisations',
+  templateUrl: './bus-organisations.component.html',
+  styleUrls: ['./bus-organisations.component.scss']
+})
+export class BusOrganisationsComponent implements OnInit {
+
+  busReportsData = [];
+  constructor(private busReportsService: BusReportsService) { }
+
+  ngOnInit(): void {
+    this.busReportsService.getBusServices()
+        .subscribe((busOrganisations: BusOrganisations) => {
+          this.busReportsData = busOrganisations.data;
+        }, () => {});
+  }
+}
